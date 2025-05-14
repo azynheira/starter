710fe2a4ee84ca6f99e6f3d9ba150f1f5122606c
Pedro Gomes <pedro.gomes@nxp.com>
2025-05-14 17:56:29 +0100

    Some moving around of configuration.

--- lua/plugins/nvim-general.lua
+++ lua/plugins/nvim-general.lua
@@ -3,45 +3,4 @@ return {
   { "snacks.nvim",
     opts = { words = { enabled = false } }
   },
-  { "folke/trouble.nvim",
-    opts = { use_diagnostic_signs = true,
-      modes = {
-        preview_float = {
-          mode = "diagnostics",
-          preview = {
-            type = "float",
-            relative = "editor",
-            border = "rounded",
-            title = "Preview",
-            title_pos = "center",
-            position = { 0, -2 },
-            size = { width = 0.3, height = 0.3 },
-            zindex = 200,
-          },
-        },
-      },
-    },
-  },
-  { "echasnovski/mini.diff",
-    event = "VeryLazy",
-    keys = {
-      {
-        "<leader>go",
-        function()
-          require("mini.diff").toggle_overlay(0)
-        end,
-        desc = "Toggle mini.diff overlay",
-      },
-    },
-    opts = {
-      view = {
-        style = "sign",
-        signs = {
-          add = "▎",
-          change = "▎",
-          delete = "",
-        },
-      },
-    },
-  }
 }
