efe15196377ed4626eeec4b46b4026285ffdb509
Pedro Gomes <pedro.gomes@nxp.com>
2025-05-14 14:17:43 +0100

    Some moving around of configuration.

--- lua/plugins/nvim-ui.lua
+++ lua/plugins/nvim-ui.lua
@@ -2,6 +2,7 @@ return {
   -- Colorschemes
   { "ellisonleao/gruvbox.nvim" },
   { "shaunsingh/nord.nvim" },
+  { "folke/tokyonight.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
@@ -74,5 +75,81 @@ return {
         end,
       })
     end,
+  },
+  {
+    "echasnovski/mini.icons",
+    opts = {
+      file = {
+        [".chezmoiignore"] = { glyph = "", hl = "MiniIconsGrey" },
+        [".chezmoiremove"] = { glyph = "", hl = "MiniIconsGrey" },
+        [".chezmoiroot"] = { glyph = "", hl = "MiniIconsGrey" },
+        [".chezmoiversion"] = { glyph = "", hl = "MiniIconsGrey" },
+        ["bash.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+        ["json.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+        ["ps1.tmpl"] = { glyph = "󰨊", hl = "MiniIconsGrey" },
+        ["sh.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+        ["toml.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+        ["yaml.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+        ["zsh.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+      },
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.sections.lualine_c = { "filename", "lsp_progress" } -- Add LSP progress
+      -- opts.sections.lualine_b = { "navic", color_correction = "dynamic" }
+    end,
+  },
+  {
+    "RRethy/vim-illuminate",
+    event = "LazyFile",
+    opts = {
+      delay = 200,
+      large_file_cutoff = 2000,
+      large_file_overrides = {
+        providers = { "lsp" },
+      },
+    },
+    config = function(_, opts)
+      require("illuminate").configure(opts)
+
+      Snacks.toggle({
+        name = "Illuminate",
+        get = function()
+          return not require("illuminate.engine").is_paused()
+        end,
+        set = function(enabled)
+          local m = require("illuminate")
+          if enabled then
+            m.resume()
+          else
+            m.pause()
+          end
+        end,
+      }):map("<leader>ux")
+
+      local function map(key, dir, buffer)
+        vim.keymap.set("n", key, function()
+          require("illuminate")["goto_" .. dir .. "_reference"](false)
+        end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+      end
+
+      map("]]", "next")
+      map("[[", "prev")
+
+      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          local buffer = vim.api.nvim_get_current_buf()
+          map("]]", "next", buffer)
+          map("[[", "prev", buffer)
+        end,
+      })
+    end,
+    keys = {
+      { "]]", desc = "Next Reference" },
+      { "[[", desc = "Prev Reference" },
+    },
   },
 }
