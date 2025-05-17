f635f1802a10b5d67a944830310f3615b74fd314
Pedro Gomes <azynheira@gmail.com>
2025-05-17 16:35:37 +0100

    Added guard for c++ formatting

--- lua/plugins/nvim-mason.lua
+++ lua/plugins/nvim-mason.lua
@@ -31,4 +31,20 @@ return {
       }
     end,
   },
+  {
+    "nvimdev/guard.nvim",
+    event = "BufReadPre",
+    config = function()
+      local ft = require("guard.filetype")
+
+      ft("c,cpp,json"):fmt("clang-format")
+
+      require("guard").setup({
+        -- the only options for the setup function
+        fmt_on_save = true,
+        -- Use lsp if no formatter was defined for this filetype
+        lsp_as_default_formatter = false,
+      })
+    end,
+  },
 }
