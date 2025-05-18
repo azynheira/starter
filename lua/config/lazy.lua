3c332aec1324e690c2219fa8eb5142d9f2f5ac4c
Pedro Gomes <azynheira@gmail.com>
2025-05-18 16:03:42 +0100

    Minor cleanup.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,45 +20,40 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "plugins" },
   },
-  performance = {
-	  cache = {
-		  enabled = true,
-	  }
-  },
   defaults = {
-	  -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-	  -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-	  lazy = false,
-	  -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-	  -- have outdated releases, which may break your Neovim install.
-	  version = false, -- always use the latest git commit
-	  -- version = "*", -- try installing the latest stable version for plugins that support semver
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
-	  enabled = true, -- check for plugin updates periodically
-	  notify = true, -- notify on update
+    enabled = true, -- check for plugin updates periodically
+    notify = true, -- notify on update
   }, -- automatically check for plugin updates
   performance = {
-	  rtp = {
-		  -- disable some rtp plugins
-		  disabled_plugins = {
-			  "gzip",
-			  "matchit",
-			  "matchparen",
-			  "netrwPlugin",
-			  "rplugin",
-			  "tarPlugin",
-			  "tohtml",
-			  "tutor",
-			  "zipPlugin",
-		  },
-	  },
+    enabled = true,
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "rplugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
   },
 })
