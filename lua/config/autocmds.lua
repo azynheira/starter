8a13945c09f5a5762d0d4f413a3db3485c9cf00f
Pedro Gomes <azynheira@gmail.com>
2025-05-19 20:22:39 +0100

    Minor cleanup. Added some code to show diagnostics.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,3 +12,10 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt.conceallevel = 0
   end,
 })
+
+-- Show errors and warnings in a floating window
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focusable = false, source = "if_many" })
+  end,
+})
--- lua/plugins/nvim-mason.lua
+++ lua/plugins/nvim-mason.lua
@@ -15,20 +15,4 @@ return {
     "mason-org/mason-lspconfig.nvim",
     version = "^1.0.0",
   },
-  {
-    "neovim/nvim-lspconfig",
-    opts = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = {
-        "<leader>cr",
-        function()
-          local inc_rename = require("inc_rename")
-          return ":" .. inc_rename.config.cmd_name .. " " .. vim.fn.expand("<cword>")
-        end,
-        expr = true,
-        desc = "Rename (inc-rename.nvim)",
-        has = "rename",
-      }
-    end,
-  },
 }
