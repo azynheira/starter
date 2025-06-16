3a239ac71c2adb0ab52ba82ce4cfb3b6f0e080a6
Pedro Gomes <pedro.gomes@nxp.com>
2025-06-16 11:39:47 +0100

    Some fixes.

--- lua/plugins/nvim-lazyvim.lua
+++ lua/plugins/nvim-lazyvim.lua
@@ -1,6 +1,45 @@
 -- nvim-lazyvim.lua Lazyvim special setup
--- Last Changed:2025-06-08 17:37:41
+-- Last Changed:2025-06-16 11:30:12
 return {
   { "folke/lazy.nvim", version = false },
   { "LazyVim/LazyVim", version = false },
+  {
+    "rootiest/nvim-updater.nvim",
+    version = "*", -- Pin to GitHub releases
+    config = function()
+      require("nvim_updater").setup({
+        source_dir = "~/.local/src/neovim", -- Custom target directory
+        build_type = "RelWithDebInfo", -- Set the desired build type
+        branch = "master", -- Track nightly branch
+        check_for_updates = true, -- Enable automatic update checks
+        notify_updates = true, -- Enables update notifications
+        default_keymaps = false, -- Disable default keymaps
+      })
+    end,
+    keys = { -- Custom keymappings
+      { -- Custom Update Neovim
+        "<Leader>cu",
+        desc = "Update Neovim",
+      },
+      { -- Custom Update Neovim
+        "<Leader>cuU",
+        function()
+          require("nvim_updater").update_neovim()
+        end,
+        desc = "Custom Update Neovim",
+      },
+      { -- Debug Build Neovim
+        "<Leader>cuD",
+        function()
+          require("nvim_updater").update_neovim({ build_type = "Debug" })
+        end,
+        desc = "Debug Build Neovim",
+      },
+      { -- Remove Neovim Source
+        "<Leader>cuR",
+        ":NVUpdateRemoveSource<CR>",
+        desc = "Remove Neovim Source Directory",
+      },
+    },
+  },
 }
